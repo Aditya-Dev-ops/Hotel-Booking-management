1a46c00caf34e39dcd2c3877c5ad8b05314a1b0e
Aditya-Dev-ops <aditya.developer.33@gmail.com>
2025-07-03 12:05:52 +0530

    Commit for saved client side code

--- client/Readme.md
+++ client/Readme.md
@@ -1,5 +1,3 @@
 This is a Hotel Booking Management Application 
 This is only a client side application built in react 
 and use tanstack query to fetch and manage data
-
-
--- client/src/services/supabase.js
+++ client/src/services/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
- export const supabaseUrl = 'https://fuqkscoqzunexywvjoma.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1cWtzY29xenVuZXh5d3Zqb21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0NjE0MDAsImV4cCI6MjAyMzAzNzQwMH0.nSeSPzaqAtQ7v2yKFNrpJmWIioZCmUvuQG9NtKJGxMM";
+ export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
